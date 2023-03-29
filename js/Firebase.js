ec3b095b6de9e67a5f2e86208aa16db8aacd5eab
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2023-03-29 21:53:36 +0530

    firebase-config

---
+++ js/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCSZalLT1a85OsKAiN6MUuwt7GJ7AaKf4M",
+    authDomain: "hound-heaven.firebaseapp.com",
+    projectId: "hound-heaven",
+    storageBucket: "hound-heaven.appspot.com",
+    messagingSenderId: "502798814366",
+    appId: "1:502798814366:web:2f669b7817abb2198ebf8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
