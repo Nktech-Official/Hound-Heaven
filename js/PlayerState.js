c8fa0936dede3c43549af31a062db40ca5981c97
Nitesh Kumar Mishra <nk.technical.org@gmail.com>
2025-02-28 20:59:31 +0530

    Update PlayerState.js

--- js/PlayerState.js
+++ js/PlayerState.js
@@ -1,4 +1,3 @@
-import AnimationState from './SpriteAnimationState.json' assert {type:'json'} ;
 import { Dust, Fire, Splash } from './particle.js';
 const states = {
     SITTING: 0,
@@ -11,7 +10,48 @@ const states = {
 
 
 }
-
+const AnimationState = [
+    {
+        "name": "idle",
+        "frames": 7
+    },
+    {
+        "name": "jump",
+        "frames": 7
+    },
+    {
+        "name": "fall",
+        "frames": 7
+    },
+    {
+        "name": "run",
+        "frames": 9
+    },
+    {
+        "name": "dizzy",
+        "frames": 11
+    },
+    {
+        "name": "sit",
+        "frames": 5
+    },
+    {
+        "name": "roll",
+        "frames": 7
+    },
+    {
+        "name": "bite",
+        "frames": 7
+    },
+    {
+        "name": "ko",
+        "frames": 12
+    },
+    {
+        "name": "gethit",
+        "frames": 4
+    }
+];
 class State {
     constructor(state, game) {
         this.state = state;
